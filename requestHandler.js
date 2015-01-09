892f175d20d217d002a8a30c1b04481445f195db
MHandi <mylesmeng@gmail.com>
2015-01-09 16:54:26 +0000

    upadta

--- requestHandler.js
+++ requestHandler.js
@@ -2,6 +2,7 @@
 var querystring = require("querystring");
 var fs = require("fs");
 
+var formidable = require("formidable");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called");
@@ -23,20 +24,32 @@ function start(response, postData) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
-    //});
 }
 
-function upload(response,postData) {
+function upload(response,request) {
 
     console.log("Request handler 'upload' was called ");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You have sent data of :" + querystring.parse(postData).text);
 
-    response.end();
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+
+    form.parse(request,function(error,fields,files){
+        console.log("parsing done");
+        fs.renameSync(files.upload.path,"/Users/Yujie/Documents/test.png");
+        response.writeHead(200,{"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    })
+
+    //response.writeHead(200, {"Content-Type": "text/plain"});
+    //response.write("You have sent data of :" + querystring.parse(postData).text);
+    //
+    //response.end();
 
 }
 
-function show (response,postData){
+function show (response){
     console.log("Request handler 'show' was called.");
     fs.readFile("/Users/Yujie/Documents/test.png","binary",function(error,file){
         if(error){
--- server.js
+++ server.js
@@ -8,17 +8,19 @@ function start(route, handle){
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + "    has received");
 
-        request.setEncoding("utf8");
+        //request.setEncoding("utf8");
 
-        request.addListener("data", function (postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '" +
-            postDataChunk + "'.");
-        });
+        //request.addListener("data", function (postDataChunk) {
+        //    postData += postDataChunk;
+        //    console.log("Received POST data chunk '" +
+        //    postDataChunk + "'.");
+        //});
+        //
+        //request.addListener("end", function () {
+        //    route(handle, pathname, response, postData);
+        //});
 
-        request.addListener("end", function () {
-            route(handle, pathname, response, postData);
-        });
+        route(handle,pathname,response,request);
     }
     http.createServer(onRequest).listen(8888);
     console.log("Server has started");
